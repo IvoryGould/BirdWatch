85b60d713dc8670835adb7e183082d78c99131c0
IvoryGould <ivory.gould@gmail.com>
2019-06-06 16:57:22 +1000

    push VRnoot

---
+++ VR-noot/Assets/lookareatest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookareatest : MonoBehaviour
+{
+
+    public Camera cam;
+    public Material redMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            if (hit.transform.tag == "bird") {
+                if (hit.collider.gameObject.GetComponent<MeshRenderer>().material.color != Color.cyan)
+                {
+                    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.cyan;
+                }
+                else if (hit.collider.gameObject.GetComponent<MeshRenderer>().material.color == Color.cyan) {
+
+                    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+
+                }
+            }
+    }
+}
---
+++ VR-noot/Assets/uitest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uitest : MonoBehaviour
+{
+
+    public GameObject testText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        testText.GetComponent<Text>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+    public void TestTextActivate() {
+
+        testText.GetComponent<Text>().enabled = true;
+
+    }
+
+}
