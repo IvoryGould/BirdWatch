4f0e7f3efad9f979c5762538411cb9d28659456d
IvoryGould <ivory.gould@gmail.com>
2019-06-13 16:21:59 +1000

    camera improvements

---
+++ VR-noot/Assets/Scripts/Managers/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+     
+    public GameObject Camera;
+    //private GameObject _wpMenu, _wpStage;
+
+    public void MovePlayerToStage(GameObject _wpStage)
+    {
+        Camera.transform.rotation = _wpStage.transform.rotation;
+    }
+
+    public void MovePlayerToMenu(GameObject _wpHome)
+    {
+        Camera.transform.position = _wpHome.transform.position;
+    }
+}
---
+++ VR-noot/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Player Locations")]
+    [SerializeField]
+        private GameObject _wpHome;
+    [SerializeField]
+        private GameObject _wpStage;
+    
+    public static bool _isPlaying;
+
+    UIController UIController;
+    CameraController CameraController;
+
+    void Awake()
+    {
+        _isPlaying = false;
+
+        UIController = GameObject.Find("MENUS Controller").GetComponent<UIController>();
+        CameraController = GameObject.Find("CAMERA Controller").GetComponent<CameraController>();
+
+    }
+
+    public void PlayGame()
+    {
+        //Debug.Log("FAKESTART GAME");
+        UIController.CvsMainMenu.enabled = false;
+        CameraController.MovePlayerToStage(_wpStage);
+    }
+}
